2b67992c02413acde8e8417865aec3f1c52d1ffb
Takoyaki <147054812+takoyakidath@users.noreply.github.com>
2025-05-03 09:38:43 +0900

    Update index.js

--- kubejs/server_scripts/remove/index.js
+++ kubejs/server_scripts/remove/index.js
@@ -3,7 +3,8 @@ const removes = [
   "mekanism:fuelwood_heater",
   "mekanism:resistive_heater", //なんで消すの...?熱源どうするの...?
   "industrialforegoing:ore_laser_base",
-  "mekanismgenerators:wind_generator", //これもなぜ消すの？
+  "mekanismgenerators:wind_generator", 
+  "mob_grinding_utils:mob_swab"
 ];
 ServerEvents.recipes((event) => {
   removes.forEach((e) => event.remove({ output: e }));
